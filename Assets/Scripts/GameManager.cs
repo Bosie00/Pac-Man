0b01ce62212c5b669aad4ff9086093afee13125c
Bosie00 <ilaria2005.dm@gmail.com>
2022-05-27 02:43:46 +0200

    non so cosa sto facendo

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -6,19 +6,14 @@ public class GameManager : MonoBehaviour
 {
     public Pacman pacman;
     public Transform pellets;
-    private int score { get; set; }
-    private int lives { get; set; }
+    private int score;
+    private int lives;
 
     // Start is called before the first frame update
     void Start()
     {
-        NewGame();
-    }
-
-    private void NewGame()
-    {
-        SetScore(0);
-        SetLives(3);
+        score = 0;
+        lives = 3;
         Debug.Log(score);
     }
 
